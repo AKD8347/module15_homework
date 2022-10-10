5b0e9b7ab0645c6d7f44c8ae522a8db03b1f3f84
Alexandr Dmitrenko <a.akd4783@gmail.com>
2022-10-10 18:44:33 +0300

    task 3

--- js/script.js
+++ js/script.js
@@ -19,3 +19,51 @@ alertTrigger.addEventListener('click', () => {
     //выводим данные через alert
     window.alert(`Высота: ${screenWidth}, Ширина: ${screenHeight}`);
 })
+
+//Задание 3
+//
+const output = document.getElementById("output");
+const field = document.querySelector('.chat__field');
+const sendMsg = document.querySelector('.chat__submit');
+const webSocUri = 'wss://echo-ws-service.herokuapp.com';
+let websocket;
+
+function writeMy(message) {
+    let pre = document.createElement("p");
+    pre.classList.add('chat__msg')
+    pre.innerHTML = message;
+    output.appendChild(pre);
+}
+
+function writeResponse(message) {
+    let pre = document.createElement("p");
+    pre.classList.add('chat__msg', 'chat__msg--answer')
+    pre.innerHTML = message;
+    output.appendChild(pre);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    websocket = new WebSocket(webSocUri);
+    websocket.onopen = function(evt) {
+        console.log('connected');
+    };
+    websocket.onclose = function(evt) {
+        // console.log(websocket);
+    };
+    websocket.onmessage = function(evt) {
+        writeResponse(evt.data);
+    };
+    websocket.onerror = function(evt) {
+        // console.log(websocket);
+    };
+})
+
+sendMsg.addEventListener('click', () => {
+    let fieldVal = field.value;
+    if(fieldVal !== '') {
+        writeMy(fieldVal);
+        websocket.send(fieldVal);
+    } else {
+        event.preventDefault();
+    }
+})
