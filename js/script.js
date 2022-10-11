bfbc9742fb9dc03fa06e183ec9df45648c4b0c68
Alexandr Dmitrenko <a.akd4783@gmail.com>
2022-10-11 12:41:03 +0300

    task 3

--- js/script.js
+++ js/script.js
@@ -21,49 +21,68 @@ alertTrigger.addEventListener('click', () => {
 })
 
 //Задание 3
-//
+//поле для вывода сообщений
 const output = document.getElementById("output");
+//поле отправляемого сообщения
 const field = document.querySelector('.chat__field');
+//кнопака отправки сообщения на сервер
 const sendMsg = document.querySelector('.chat__submit');
+//url эхо-сервера
 const webSocUri = 'wss://echo-ws-service.herokuapp.com';
+//переменная для вэбсокета
 let websocket;
-
+//функция записи отправленного сообщения
 function writeMy(message) {
+    //в созданный тег p
     let pre = document.createElement("p");
+    //добавляем нужный класс
     pre.classList.add('chat__msg')
+    //добавляем верстку
     pre.innerHTML = message;
+    //заполненный элемент добавляем в поле сообщений
     output.appendChild(pre);
 }
-
+//функция записи ответа сервера
 function writeResponse(message) {
+    //в созданный тег p
     let pre = document.createElement("p");
+    //добавляем нужный класс
     pre.classList.add('chat__msg', 'chat__msg--answer')
+    //добавляем верстку
     pre.innerHTML = message;
+    //заполненный элемент добавляем в поле сообщений
     output.appendChild(pre);
 }
-
+//при загрузке страницы
 document.addEventListener("DOMContentLoaded", function () {
+    //устанавливаем соединение через вебсокет с сервером
     websocket = new WebSocket(webSocUri);
     websocket.onopen = function(evt) {
         console.log('connected');
     };
     websocket.onclose = function(evt) {
-        // console.log(websocket);
+        console.log('connected');
     };
     websocket.onmessage = function(evt) {
+        //получаем ответ от сервера и передаем его в функцию записи ответа
         writeResponse(evt.data);
     };
     websocket.onerror = function(evt) {
-        // console.log(websocket);
+        console.log('error');
     };
 })
-
+//при клике на кнопку отправки сообщения
 sendMsg.addEventListener('click', () => {
+    //берем значение поля
     let fieldVal = field.value;
+    //если поле не пустое
     if(fieldVal !== '') {
+        //записываем сообщение
         writeMy(fieldVal);
+        //и отправляем его на сервер
         websocket.send(fieldVal);
     } else {
+        //если пустое - ничего не отправляется
         event.preventDefault();
     }
 })
